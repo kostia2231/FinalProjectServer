7b79fbaa7427e72b1cd4d3b8d132819ddffd934c
konstantin <kostia505@gmail.com>
2025-01-09 00:11:27 +0100

    db connection: added

---
+++ src/config/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const uri: string | undefined = process.env.URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri || "");
+    console.log("DB connected successfully");
+  } catch (err) {
+    console.error(`DB connection failed: ${(err as Error).stack}`);
+  }
+};
+
+export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
 import "dotenv/config";
 
 class Server {
@@ -10,11 +11,20 @@ class Server {
     this.app = express();
     this.port = Number(process.env.PORT) || 8080;
     this.Middlewares();
+    this.InitDB();
   }
 
   private Middlewares() {
     this.app.use(express.json());
     this.app.use(cors());
+  }
+
+  private async InitDB() {
+    try {
+      await connectDB();
+    } catch (err) {
+      console.error(`DB connection failed: ${(err as Error).stack}`);
+    }
   }
 
   public startServer() {
