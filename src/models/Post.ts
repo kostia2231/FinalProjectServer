02a418bc246f49ab8deee657edd20aa734a23902
konstantin <kostia505@gmail.com>
2025-01-09 15:30:14 +0100

    models: fixed, Post: added

---
+++ src/models/Post.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document, Types } from "mongoose";
+
+interface IPost extends Document {
+  userId: Types.ObjectId;
+  imgUrls: string[];
+  caption: string;
+  likesCount: number;
+  likes: Types.ObjectId[];
+  commentsCount: number;
+  comments: Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const PostSchema = new Schema<IPost>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    imgUrls: [{ type: String, required: true }],
+    caption: { type: String, default: "" },
+    likesCount: { type: Number, default: 0 },
+    likes: [{ type: Schema.Types.ObjectId, ref: "Like", required: true }],
+    commentsCount: { type: Number, default: 0 },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment", required: true }],
+  },
+  { timestamps: true },
+);
+
+const PostModel = model("Post", PostSchema);
+export default PostModel;
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,20 +1,38 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document, Types } from "mongoose";
 
 interface IUser extends Document {
   username: string;
   email: string;
   fullName: string;
   password: string;
-  createdAt: NativeDate;
-  updatedAt: NativeDate;
+  bio: string;
+  website: string;
+  profile_img: string;
+  followersCount: number;
+  followers: Types.ObjectId[];
+  followingCount: number;
+  following: Types.ObjectId[];
+  postsCount: number;
+  posts: Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const UserSchema = new Schema<IUser>(
   {
-    username: { type: String, required: true, unique: true, trim: true },
-    email: { type: String, required: true, unique: true, trim: true },
-    fullName: { type: String, required: true, trim: true },
-    password: { type: String, required: true, trim: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    fullName: { type: String, required: true },
+    password: { type: String, required: true },
+    bio: { type: String, default: "" },
+    website: { type: String, default: "" },
+    profile_img: { type: String, default: "" },
+    followersCount: { type: Number, default: 0 },
+    followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    followingCount: { type: Number, default: 0 },
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    postsCount: { type: Number, default: 0 },
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   },
   { timestamps: true },
 );
