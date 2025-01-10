56b3252d58a3579f0e26ddae06560152506800db
konstantin <kostia505@gmail.com>
2025-01-10 11:19:22 +0100

    auth middlware: added

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import { Response, Request, NextFunction } from "express";
+import { TokenPayload } from "../types/express.js";
+
+export function ifAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res
+      .status(401)
+      .json({ message: "authorization header is required or wrong format" });
+    return;
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    console.error("JWT secret key is missing");
+    res.status(500).json({ message: "Internal server error" });
+    return;
+  }
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      console.error(`JWT verification failed: ${(err as Error).message}`);
+      res.status(403).json({ message: "invalid or expired token" });
+      return;
+    }
+
+    req.user = decoded as TokenPayload;
+    next();
+  });
+}
---
+++ src/types/express.d.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+export interface TokenPayload {
+  username: string;
+  id: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: TokenPayload;
+    }
+  }
+}
